3d14c413153409dd9fbe88d9c9687b837b6e23c9
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-05 18:59:35 -0800

    add loop

---
+++ 10DaysOfJavascript/Day2_Loops.js
@@ -0,0 +1,52 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+  const arr1 = [];
+  const arr2 = [];
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s[i] === "a" ||
+      s[i] === "e" ||
+      s[i] === "i" ||
+      s[i] === "o" ||
+      s[i] === "u"
+    ) {
+      arr1.push(s[i]);
+    } else {
+      arr2.push(s[i]);
+    }
+  }
+  const arr3 = arr1.concat(arr2);
+  for (let i = 0; i < arr3.length; i++) {
+    console.log(arr3[i]);
+  }
+}
